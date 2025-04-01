47e9e3f7847e587fb1a57b7b56e2af181c6863b6
Arthur <thurdelima@gmail.com>
2025-04-01 18:29:04 -0300

    first commit

---
+++ pages/2_🏃_players.py
@@ -0,0 +1,51 @@
+import streamlit as st
+import pandas as pd
+from datetime import datetime
+
+st.set_page_config(
+        page_title="FIFA Analytics",
+        page_icon="⚽",
+         layout="wide",
+    )
+
+if "data" not in st.session_state:
+    df_data = pd.read_csv("datasets/CLEAN_FIFA23_official_data.csv", index_col=0)
+    df_data = df_data[df_data["Contract Valid Until"] >= datetime.today().year]
+    df_data = df_data[df_data["Value(£)"] > 0]
+    df_data = df_data.sort_values(by="Overall", ascending=False)
+    st.session_state["data"] = df_data
+
+
+
+df_data = st.session_state["data"]
+
+
+clubes = df_data["Club"].value_counts().index
+club = st.sidebar.selectbox("Clube", clubes)
+
+df_players = df_data[(df_data["Club"] == club)]
+players = df_players["Name"].value_counts().index
+player = st.sidebar.selectbox("Jogador", players)
+
+
+player_stats = df_data[df_data["Name"] == player].iloc[0]
+
+
+st.image([player_stats["Photo"], player_stats["Club Logo"]])
+#col2.image(player_stats["Photo"])
+
+st.title(player_stats["Name"])
+
+col1, col2, col3, col4 = st.columns(4)
+col1.markdown(f"**Idade:** {player_stats['Age']}")
+col2.markdown(f"**Altura:** {player_stats['Height(cm.)'] / 100}")
+col3.markdown(f"**Peso:** {player_stats['Weight(lbs.)']*0.453:.2f}")
+st.divider()
+
+st.subheader(f"Overall {player_stats['Overall']}")
+st.progress(int(player_stats["Overall"]))
+
+col1, col2, col3, col4 = st.columns(4)
+col1.metric(label="Valor de mercado", value=f"£ {player_stats['Value(£)']:,}")
+col2.metric(label="Remuneração semanal", value=f"£ {player_stats['Wage(£)']:,}")
+col3.metric(label="Cláusula de rescisão", value=f"£ {player_stats['Release Clause(£)']:,}")
---
+++ pages/3_⚽_teams.py
@@ -0,0 +1,57 @@
+import streamlit as st
+import pandas as pd
+from datetime import datetime
+
+st.set_page_config(
+        page_title="FIFA Analytics",
+        page_icon="⚽",
+         layout="wide",
+    )
+
+if "data" not in st.session_state:
+    df_data = pd.read_csv("datasets/CLEAN_FIFA23_official_data.csv", index_col=0)
+    df_data = df_data[df_data["Contract Valid Until"] >= datetime.today().year]
+    df_data = df_data[df_data["Value(£)"] > 0]
+    df_data = df_data.sort_values(by="Overall", ascending=False)
+    st.session_state["data"] = df_data
+
+
+
+
+
+df_data = st.session_state["data"]
+
+
+clubes = df_data["Club"].value_counts().index
+club = st.sidebar.selectbox("Clube", clubes)
+
+df_filtered = df_data[(df_data["Club"] == club)].set_index("Name")
+
+st.image(df_filtered.iloc[0]["Club Logo"])
+st.markdown(f"## {club}")
+
+
+columns = ["Age", "Photo", "Flag", "Overall", "Value(£)", "Wage(£)", "Joined", "Height(cm.)", "Weight(lbs.)", "Contract Valid Until", "Release Clause(£)"]
+
+
+
+st.dataframe(df_filtered[columns], column_config={
+    "Overall": st.column_config.ProgressColumn(
+        "Overall", format="%d", min_value=0, max_value=100
+    ),
+    "Wage(£)": st.column_config.ProgressColumn("Weekly Wage", format="£%f", min_value=0, max_value=df_filtered["Wage(£)"].max()),
+    "Photo": st.column_config.ImageColumn(),
+     "Flag": st.column_config.ImageColumn("Country"),
+})
+
+
+
+
+
+
+
+
+
+
+
+
